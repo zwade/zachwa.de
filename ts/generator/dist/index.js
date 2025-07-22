afa3d8b50135623e182159be621992ede72f6015
Zachary Wade <zacharywade@gmail.com>
2025-07-21 17:44:28 -0700

    Update blog

--- ts/generator/dist/index.js
+++ ts/generator/dist/index.js
@@ -1,1 +1,44 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("node:path");
+const marked = require("marked");
+const sanitize = require("sanitize-html");
+const fetch = require("isomorphic-fetch");
+const fs = require("node:fs/promises");
+;
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    const dttwRequest = yield fetch("https://dttw.tech/api/author/zach");
+    const dttwResponse = yield dttwRequest.json();
+    const dttwPosts = dttwResponse
+        .map(({ timestamp, title: { text: title, url }, content }) => {
+        const asStandardMarkdown = content
+            .replace(/\^\^\^/g, "")
+            .replace(/:::aside(.|\n)+:::/g, "");
+        const asHTML = marked.marked(asStandardMarkdown);
+        const html = sanitize(asHTML);
+        return { timestamp: Math.floor(timestamp / 1000), title, url: `https://dttw.tech${url}`, html, kind: "dttw" };
+    });
+    const wishRequest = yield fetch("https://compassrx.dev/api/posts.json");
+    const wishResponse = yield wishRequest.json();
+    const wishPosts = wishResponse.map(({ id, slug, title, description, date }) => {
+        return {
+            timestamp: Math.floor(new Date(date).getTime() / 1000),
+            title,
+            url: `https://compassrx.dev/blog/${id}/${slug}`,
+            html: description,
+            kind: "wish"
+        };
+    });
+    const allPosts = [...dttwPosts, ...wishPosts].sort((a, b) => b.timestamp - a.timestamp);
+    yield fs.writeFile(path.join(__dirname, "../../../_data/all_posts.json"), JSON.stringify(allPosts));
+});
+main();
--- ts/generator/src/index.ts
+++ ts/generator/src/index.ts
@@ -1,9 +1,9 @@
-import * as path from "path";
+import * as path from "node:path";
 
 import * as marked from "marked";
 import * as sanitize from "sanitize-html";
 import * as fetch from "isomorphic-fetch";
-import * as fs from "fs-extra";
+import * as fs from "node:fs/promises";
 
 interface DttwPost {
     timestamp: number;
@@ -16,31 +16,59 @@ interface DttwPost {
     content: string;
 }
 
+interface WishPost {
+    id: string;
+    slug: string;
+    title: string;
+    body: string;
+    image: string;
+    description: string;
+    date: string;
+    kind: string;
+    author: string;
+    ogImage?: string;
+    hidden?: boolean;
+}
+
 export interface SanitizedPost {
     timestamp: number;
     title: string;
     url: string;
     html: string;
+    kind: "dttw" | "wish"
 };
 
 const main = async () => {
     const dttwRequest = await fetch("https://dttw.tech/api/author/zach");
-    const apiResponse: DttwPost[] = await dttwRequest.json();
+    const dttwResponse: DttwPost[] = await dttwRequest.json();
 
-    const withHTML = apiResponse
+    const dttwPosts = dttwResponse
         .map(({ timestamp, title: { text: title, url }, content }): SanitizedPost => {
             const asStandardMarkdown = content
                 .replace(/\^\^\^/g, "")
                 .replace(/:::aside(.|\n)+:::/g, "");
-            console.log(asStandardMarkdown);
-            const asHTML = marked(asStandardMarkdown);
+            const asHTML = marked.marked(asStandardMarkdown) as string;
             const html = sanitize(asHTML);
 
-            return { timestamp: Math.floor(timestamp / 1000), title, url, html };
+            return { timestamp: Math.floor(timestamp / 1000), title, url: `https://dttw.tech${url}`, html, kind: "dttw" };
         })
-        .sort(({ timestamp: t1 }, { timestamp: t2 }) => t2 - t1);
 
-    await fs.writeFile(path.join(__dirname, "../../../_data/dttw_posts.json"), JSON.stringify(withHTML));
+
+    const wishRequest = await fetch("https://compassrx.dev/api/posts.json");
+    const wishResponse: WishPost[] = await wishRequest.json();
+    const wishPosts = wishResponse.map(({ id, slug, title, description, date }): SanitizedPost => {
+        return {
+            timestamp: Math.floor(new Date(date).getTime() / 1000),
+            title,
+            url: `https://compassrx.dev/blog/${id}/${slug}`,
+            html: description,
+            kind: "wish"
+        }
+    })
+
+    const allPosts = [...dttwPosts, ...wishPosts].sort((a, b) => b.timestamp - a.timestamp);
+
+    await fs.writeFile(path.join(__dirname, "../../../_data/all_posts.json"), JSON.stringify(allPosts));
 }
 
 main();
